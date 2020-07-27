54376e8ec8f173544780e8381af00888f7718f4a
Martin Shinebaum <mshinebaum@gmail.com>
2020-07-27 13:31:52 +0100

    Restricted access to logged in users

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'TasksController@index');
+
 
 Auth::routes();
 
-Route::get('/task','TasksController@add');
-Route::post('/task','TasksController@create');
-
-Route::get('/task/{task}','TasksController@edit');
-Route::post('/task/{task}','TasksController@update');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', 'TasksController@index');
+    Route::get('/task','TasksController@add');
+    Route::post('/task','TasksController@create');
+    Route::get('/task/{task}','TasksController@edit');
+    Route::post('/task/{task}','TasksController@update');
+});
